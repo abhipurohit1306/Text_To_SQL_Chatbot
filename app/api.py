29d4e82aeacda08d88213984b27b9ab61efefac7
Abhishek Purohit <apurohit1306@gmail.com>
2026-06-26 18:17:31 +0530

    FastAPI backend with chat endpoint

---
+++ app/api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.models import ChatRequest
+from app.chatbot import process_question
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "API Running"}
+
+@app.post("/chat")
+def process(request: ChatRequest):
+    return process_question(request.question)
